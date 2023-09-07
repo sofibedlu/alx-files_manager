e29f28e97125a57805d06213393b68c857357c5a
sofy <bedlusofi@gmail.com>
2023-09-07 09:31:33 +0300

    check-for-invalid-base64

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -2,7 +2,6 @@ import crypto from 'crypto';
 import { v4 as uuidv4 } from 'uuid';
 import redisClient from '../utils/redis';
 import dbclient from '../utils/db';
-import isValidbase64 from '../utils/auth';
 
 const AuthController = {
   getConnect: async (req, res) => {
@@ -16,34 +15,37 @@ const AuthController = {
 
       // Extract and decode the base64 credentials
       const base64Credentials = authHeader.split(' ')[1];
-      if (!isValidbase64(base64Credentials)) {
-        return res.status(401).json({ error: 'Unauthorized' });
+
+      try {
+        const credentials = Buffer.from(base64Credentials, 'base64').toString('utf-8');
+        const [email, password] = credentials.split(':');
+
+        // Hash the password using SHA1
+        const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
+
+        // Find the user in the database
+        const user = await dbclient.client.db().collection('users')
+          .findOne({ email, password: hashedPassword });
+
+        if (!user) {
+          return res.status(401).json({ error: 'Unauthorized' });
+        }
+
+        // Generate a random token using uuidv4
+        const token = uuidv4();
+
+        // Create a Redis key for the user's token
+        const redisKey = `auth_${token}`;
+
+        // Store the user's ID in Redis with a 24-hour expiration
+        await redisClient.set(redisKey, user._id.toString(), 24 * 60 * 60);
+
+        // Return the generated token
+        res.status(200).json({ token });
+      } catch (decodeError) {
+        // Handle invalid Base64 content
+        return res.status(400).json({ error: 'Invalid base64 encoding' });
       }
-      const credentials = Buffer.from(base64Credentials, 'base64').toString('utf-8');
-      const [email, password] = credentials.split(':');
-
-      // Hash the password using SHA1
-      const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
-
-      // Find the user in the database
-      const user = await dbclient.client.db().collection('users')
-        .findOne({ email, password: hashedPassword });
-
-      if (!user) {
-        return res.status(401).json({ error: 'Unauthorized' });
-      }
-
-      // Generate a random token using uuidv4
-      const token = uuidv4();
-
-      // Create a Redis key for the user's token
-      const redisKey = `auth_${token}`;
-
-      // Store the user's ID in Redis with a 24-hour expiration
-      await redisClient.set(redisKey, user._id.toString(), 24 * 60 * 60);
-
-      // Return the generated token
-      res.status(200).json({ token });
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: 'Internal Server Error' });
